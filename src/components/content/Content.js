de0a1d3ad080a75ff379cd63a77c9d56b38e6eb8
Marco Steiner <marco.steiner@windowslive.com>
2022-02-14 16:54:22 +0100

    Refactoring

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,76 +1,92 @@
 import logo from "../../logo.svg";
 
+function Title() {
+    return <div className="title">
+        <h1>Marco Steiner</h1>
+        <h3>Software Engineer</h3>
+    </div>;
+}
+
+function About() {
+    return <div className="about">
+        <h3>About</h3>
+        Just a very curios engineer interested in finding out how the world works and wanting to be useful.
+    </div>;
+}
+
+function Education() {
+    return <div className="education">
+        <h3>Education</h3>
+        <table>
+            <tbody>
+            <tr>
+                <td>Now</td>
+                <td>MSc BA New Business</td>
+                <td>Chur</td>
+            </tr>
+            <tr>
+                <td>Sep 18</td>
+                <td>BSc FHO in Compuer Science</td>
+                <td>Rapperswil</td>
+            </tr>
+            <tr>
+                <td>Aug 14</td>
+                <td>Informatiker Systemtechnik EFZ</td>
+                <td>Uster/Wetzikon</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>;
+}
+
+function Timeline() {
+    return <div className="timeline">
+        <h3>Timeline</h3>
+        <table>
+            <tbody>
+            <tr>
+                <td>Now</td>
+                <td>MSc BA New Business</td>
+                <td>Chur</td>
+            </tr>
+            <tr>
+                <td>Sep 18</td>
+                <td>BSc FHO in Compuer Science</td>
+                <td>Rapperswil</td>
+            </tr>
+            <tr>
+                <td>Aug 14</td>
+                <td>Informatiker Systemtechnik EFZ</td>
+                <td>Uster/Wetzikon</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>;
+}
+
+function Projects() {
+    return <div className="projects">
+        <h3>Projects</h3>
+        <table>
+            <tbody>
+            <tr>
+                <td>Now</td>
+                <td>MSc BA New Business</td>
+                <td>Chur</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>;
+}
+
 function Content() {
     return (
         <div className="content">
-            <div className="title">
-                <h1>Marco Steiner</h1>
-                <h3>Software Engineer</h3>
-            </div>
-
-            <div className="about">
-                <h3>About</h3>
-                Just a very curios engineer interested in finding out how the world works and wanting to be useful.
-            </div>
-
-            <div className="education">
-                <h3>Education</h3>
-                <table>
-                    <tbody>
-                    <tr>
-                        <td>Now</td>
-                        <td>MSc BA New Business</td>
-                        <td>Chur</td>
-                    </tr>
-                    <tr>
-                        <td>Sep 18</td>
-                        <td>BSc FHO in Compuer Science</td>
-                        <td>Rapperswil</td>
-                    </tr>
-                    <tr>
-                        <td>Aug 14</td>
-                        <td>Informatiker Systemtechnik EFZ</td>
-                        <td>Uster/Wetzikon</td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-
-            <div className="timeline">
-                <h3>Timeline</h3>
-                <table>
-                    <tbody>
-                    <tr>
-                        <td>Now</td>
-                        <td>MSc BA New Business</td>
-                        <td>Chur</td>
-                    </tr>
-                    <tr>
-                        <td>Sep 18</td>
-                        <td>BSc FHO in Compuer Science</td>
-                        <td>Rapperswil</td>
-                    </tr>
-                    <tr>
-                        <td>Aug 14</td>
-                        <td>Informatiker Systemtechnik EFZ</td>
-                        <td>Uster/Wetzikon</td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-
-            <div className="projects">
-                <h3>Projects</h3>
-                <table>
-                    <tbody>
-                    <tr>
-                        <td>Now</td>
-                        <td>MSc BA New Business</td>
-                        <td>Chur</td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
+            {Title()}
+            {About()}
+            {Education()}
+            {Timeline()}
+            {Projects()}
         </div>
     );
 }
